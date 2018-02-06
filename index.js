45237b178041181593d8a47bc0d1c3514a624830
Pavel Kovalenko <pavel@kovalenko.cz>
2018-02-06 10:55:48 +0100

    Исправляет код на соответствие критериям

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 'use strict';
 
-const verText = 'v0.0.1';
+const VERSION_TEXT = 'v0.0.1';
 
-const helpText = `Доступные команды:
+const HELP_TEXT = `Доступные команды:
   --help    — печатает этот текст;
   --version — печатает версию приложения;`;
 
-const defaultText = `Привет! Это учебное приложение. Автор: Павел Коваленко.
+const DEFAULT_TEXT = `Привет! Это учебное приложение. Автор: Павел Коваленко.
 Используйте "--help" для того чтобы увидеть доступные команды`;
 
-let exit = (code) => {
+const exit = (code) => {
   process.exit(code);
 };
 
-let errMsg = (cmd) =>{
+const getErrMsg = (cmd) =>{
   return `Неизвестная команда ${cmd}.
 Чтобы прочитать правила использования приложения, наберите "--help"`
 };
 
-function doTask(args) {
+const doTask = (args) => {
   switch (args) {
     case '--version':
-      console.log(verText);
+      console.log(VERSION_TEXT);
       exit(0);
       break;
     case '--help':
-      console.log(helpText);
+      console.log(HELP_TEXT);
       exit(0);
       break;
     case undefined:
-      console.log(defaultText);
+      console.log(DEFAULT_TEXT);
       exit(0);
       break;
     default:
-      const err = errMsg(args);
+      const err = getErrMsg(args);
       console.error(err);
       exit(1);
       break;
   }
-}
+};
 
 doTask(process.argv[2]); // умеет обрабатывать только одну команду
