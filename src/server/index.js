82aae3e65af152241126834875b2bf1a5d6c5705
Pavel Kovalenko <pavel@kovalenko.cz>
2018-02-22 10:03:55 +0100

    Убрал директории - ибо нет в задании

--- src/server/index.js
+++ src/server/index.js
@@ -3,27 +3,11 @@ const url = require(`url`);
 const fs = require(`fs`);
 const {promisify} = require(`util`);
 const path = require(`path`);
-const stat = promisify(fs.stat);
-const readdir = promisify(fs.readdir);
-const readfile = promisify(fs.readFile);
 const config = require(`../../config`);
 const contentTypes = require(`../utils/contentTypes`);
-const HOSTNAME = `127.0.0.1`;
 
-const printDirectory = (targetPath, files) => {
-  return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Directory content</title>
-</head>
-<body>
-<ul>
-    ${files.map((it) => `<li><a href="${it}">${it}</a></li>`).join(``)}
-</ul>
-</body>
-</html>`;
-};
+const readfile = promisify(fs.readFile);
+const HOSTNAME = `127.0.0.1`;
 
 const readFile = async (targetPath, res) => {
   const data = await readfile(targetPath);
@@ -31,15 +15,6 @@ const readFile = async (targetPath, res) => {
   res.setHeader(`content-type`, contentTypes[ext]);
   res.setHeader(`content-length`, Buffer.byteLength(data));
   res.end(data);
-};
-
-
-const readDir = async (targetPath, res) => {
-  const files = await readdir(targetPath);
-  res.setHeader(`content-type`, `text/html`);
-  const content = printDirectory(targetPath, files);
-  res.setHeader(`content-length`, Buffer.byteLength(content));
-  res.end(content);
 };
 
 const server = http.createServer((req, res) => {
@@ -52,16 +27,9 @@ const server = http.createServer((req, res) => {
 
   (async () => {
     try {
-      const pathStat = await stat(absolutePath);
-
       res.statusCode = 200;
       res.statusMessage = `OK`;
-
-      if (pathStat.isDirectory()) {
-        await readDir(absolutePath, res);
-      } else {
-        await readFile(absolutePath, res);
-      }
+      await readFile(absolutePath, res);
     } catch (e) {
       res.writeHead(404, `Not Found`);
       res.end();
