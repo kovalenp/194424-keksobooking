13f36c3fcc2a431cbb580e5cb15adf8d1f159c1f
Pavel Kovalenko <pavel@kovalenko.cz>
2018-02-23 22:09:58 +0100

    Использует express.static вместо html сервера

--- src/server/index.js
+++ src/server/index.js
@@ -1,51 +1,17 @@
-const http = require(`http`);
-const url = require(`url`);
-const fs = require(`fs`);
-const {promisify} = require(`util`);
-
+const express = require(`express`);
 const config = require(`../../config`);
-const {resolve} = require(`./contentTypes`);
 
-const readfile = promisify(fs.readFile);
 const HOSTNAME = `127.0.0.1`;
 
-const readFile = async (targetPath, res) => {
-  const data = await readfile(targetPath);
-  res.setHeader(`content-type`, resolve(targetPath));
-  res.setHeader(`content-length`, data.length);
-  res.end(data);
-};
-
-const server = http.createServer((req, res) => {
-  let absolutePath;
-  if (req.url === `/`) {
-    absolutePath = config.STATIC_DIR + `/index.html`;
-  } else {
-    absolutePath = config.STATIC_DIR + url.parse(req.url).pathname;
-  }
-
-  (async () => {
-    try {
-      res.statusCode = 200;
-      res.statusMessage = `OK`;
-      await readFile(absolutePath, res);
-    } catch (e) {
-      res.writeHead(404, `Not Found`);
-      res.end();
-    }
-  })().catch((e) => {
-    res.writeHead(500, e.message, {
-      'content-type': `text/plain`
-    });
-    res.end(e.message);
-  });
-});
+const app = express();
+app.use(express.static(config.STATIC_DIR));
 
 module.exports = {
   run(port) {
     const serverAddress = `http://${HOSTNAME}:${port}`;
-    server.listen(port, HOSTNAME, () => {
+    app.listen(port, HOSTNAME, () => {
       console.log(`🌍 running at ${serverAddress}/`);
     });
-  }
+  },
+  app
 };
